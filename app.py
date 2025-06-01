16ac8ad73393364b10e3640f48842d1a1813c476
vimalchaaubey <chaubey_vimal@yahoo.com>
2025-06-01 08:16:40 +0000

    Add team registration: team name, dynamic member info, and backend support

--- app.py
+++ app.py
@@ -14,14 +14,22 @@ PROBLEM_STATEMENTS = [
 def register():
     registration = None
     if request.method == 'POST' and 'submit' in request.form:
+        team_size = int(request.form['team_size'])
+        members = []
+        for i in range(team_size):
+            members.append({
+                'first_name': request.form.get(f'first_name_{i}', ''),
+                'last_name': request.form.get(f'last_name_{i}', ''),
+                'state': request.form.get(f'state_{i}', ''),
+                'email': request.form.get(f'email_{i}', ''),
+                'institute': request.form.get(f'institute_{i}', ''),
+                'branch': request.form.get(f'branch_{i}', ''),
+                'year': request.form.get(f'year_{i}', ''),
+            })
         registration = {
-            'first_name': request.form['first_name'],
-            'last_name': request.form['last_name'],
-            'state': request.form['state'],
-            'email': request.form['email'],
-            'institute': request.form['institute'],
-            'branch': request.form['branch'],
-            'year': request.form['year'],
+            'team_name': request.form['team_name'],
+            'team_size': team_size,
+            'members': members,
             'problem': request.form['problem'],
         }
     return render_template('register.html', problems=PROBLEM_STATEMENTS, registration=registration)
